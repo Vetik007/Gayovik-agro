b618983fad9b3ee18eb5ef2d7daa508b245a1102
Vetik007 <boy0007@ukr.net>
2023-11-09 00:55:52 +0200

    update vite

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# если вы деплоите на кастомный домен
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
