8bd37d8838b960df71737c53151e9c285b777441
Vetik007 <boy0007@ukr.net>
2023-11-09 00:32:10 +0200

    update repository

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,7 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
-// ========================================
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import svgr from "vite-plugin-svgr";
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
-  resolve: {
-    alias: {
-      src: "/src",
-      components: "/src/components",
-    },
-  },
-  base: "/react_vite/",
-});
+  plugins: [react()],
+})
