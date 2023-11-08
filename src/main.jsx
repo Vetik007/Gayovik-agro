f4e7681fe14c2a749d3cb716e54ebc982bc45350
Vetik007 <boy0007@ukr.net>
2023-11-09 00:09:25 +0200

    update vit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+// ======================================================
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter basename="/react_vite">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
 // ========================================
 
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-// import svgr from "vite-plugin-svgr";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 
-// export default defineConfig({
-//   plugins: [react(), svgr()],
-//   resolve: {
-//     alias: {
-//       src: "/src",
-//       components: "/src/components",
-//     },
-//   },
-//   base: "/react_vite/",
-// });
+export default defineConfig({
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      src: "/src",
+      components: "/src/components",
+    },
+  },
+  base: "/react_vite/",
+});
