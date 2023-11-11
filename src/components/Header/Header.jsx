2240453291e3bda8a9f635ab8d652b4606f345f1
Vetik007 <boy0007@ukr.net>
2023-11-11 23:47:15 +0200

    update header(add contacts, add search form)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,51 @@
+import { BiSearch } from 'react-icons/bi';
+
 import {
   HeaderContainer,
   Navigation,
   StyledLink,
   IconWrapper,
+  HeaderContact,
+  ContactList,
+  InputStyled,
+  ButtonStyled,
+  FormStyled,
+  FormWrapper
 } from './Header.styled';
 import sprite from '../../assets/sprite.svg';
 
 export const Header = () => {
   return (
     <HeaderContainer>
+
+       <HeaderContact>
+        <ContactList>
+          <li>067-111-111-111</li>
+          <li>agro@gmail.com</li>
+          <li>Наше місцезнаходження</li>
+        </ContactList>
+      </HeaderContact>
+
+      <FormWrapper>
+        
+        <FormStyled>
+          
+
+
+          <InputStyled type="text" placeholder="Пошук за номером деталі, описом, маркою, моделлю..." />
+        <ButtonStyled type="submit">
+          <BiSearch size="24" />
+        </ButtonStyled>
+        
+        </FormStyled>
+
+      </FormWrapper>
+
+
+
+
+
+
       <Navigation>
 
         <StyledLink to="/">
--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -49,3 +49,108 @@ export const IconWrapper = styled.svg`
     0 0 40px #00baff,
     0 0 80px #00baff;
 `;
+
+
+export const HeaderContact = styled.div`
+    background-color: #000000;
+;
+    width: 1440px;
+  
+
+  
+`;
+
+export const ContactList = styled.ul`
+    display: flex;
+    width: 1440px;
+     gap: 20px;
+
+     color: #FFFFFF;
+font-family: Poppins;
+font-weight: 400;
+font-size: 12px;
+line-height: 18px;
+    
+`;
+
+export const InputStyled = styled.input`
+    width: 856px;
+    height: 40px;
+
+    display: inline-block;
+    
+  /* width: 100%; */
+  
+  font: inherit;
+font-family: Poppins;
+font-weight: 400;
+font-size: 14px;
+line-height: 24px;
+
+
+  
+  border: none;
+  outline: none;
+  /* padding-left: 16px; */
+  /* padding-right: 12px; */
+  ::placeholder {
+    font: inherit;
+    font-size: 16px;
+  }
+    
+`;
+
+export const ButtonStyled = styled.button`
+    display: inline-block;
+  width: 48px;
+  height: 48px;
+  border: 0;
+  background-size: 70%;
+  background-repeat: no-repeat;
+  background-position: center;
+  opacity: 0.6;
+  transition: opacity 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  cursor: pointer;
+  outline: none;
+  :hover {
+    opacity: 1;
+  }
+    
+`;
+
+export const FormStyled = styled.form`
+   display: flex;
+  align-items: center;
+  /* width: 100%; */
+  max-width: 856px;
+  margin-left: 250px;
+  
+  background-color: #fff;
+  border-radius: 3px;
+  overflow: hidden;
+
+padding-left: 16px;
+  padding-right: 12px;
+    
+`;
+
+export const FormWrapper = styled.div`
+width: 1440px;
+   background-color: #2B4D5E;
+   padding-top: 17px;
+   padding-bottom: 16px;
+`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
