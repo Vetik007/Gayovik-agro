a90bc3c44e81fd49cf4066e6a31c2858ce58de91
Vetik007 <boy0007@ukr.net>
2023-11-23 23:32:08 +0200

    Update HomePage.styled.jsx

--- src/pages/HomePage/HomePage.styled.jsx
+++ src/pages/HomePage/HomePage.styled.jsx
@@ -33,7 +33,6 @@ export const PromoTitle = styled.h2`
   font-family: Oswald;
   font-weight: bold;
   font-size: 16px;
-  /* line-height: 23.7px; */
   color: #c00000;
 `;
 
@@ -90,40 +89,6 @@ export const PromoButton = styled.button`
   }
 `;
 
-// position: absolute;
-//   width: 40px;
-//   height: 40px;
-//   border-color: none;
-//   border-radius: 50%;
-//   right: 16px;
-//   top: 38px;
-//   background-color: #2b4d5e;
-//   &:hover,
-//   &:focus {
-//     /* border: solid 2px;
-//     border-color: white; */
-//     transition: opasity 2000ms ease 0s;
-//   }
-
-// .btn:before {
-//   content: "";
-//   position: absolute;
-//   top: 0;
-//   left: -100%;
-//   width: 100%;
-//   height: 100%;
-//   background: linear-gradient(
-//     120deg,
-//     transparent,
-//     rgba(146, 148, 248, 0.4),
-//     transparent
-//   );
-//   transition: all 650ms;
-// }
-
-// .btn:hover:before {
-//   left: 100%;
-// }
 
 export const ButtonIcon = styled(FiArrowRight)`
   width: 16px;
@@ -131,56 +96,3 @@ export const ButtonIcon = styled(FiArrowRight)`
   color: white;
 `;
 
-/* #root {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-  text-align: center;
-} */
-
-/* .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.react:hover {
-  filter: drop-shadow(0 0 2em #61dafbaa);
-}
-
-@keyframes logo-spin {
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-}
-
-@media (prefers-reduced-motion: no-preference) {
-  a:nth-of-type(2) .logo {
-    animation: logo-spin infinite 20s linear;
-  }
-} */
-
-// .title {
-//   color: black;
-//   text-shadow:
-//     0 0 5px #00baff,
-//     0 0 10px #00baff,
-//     0 0 20px #00baff,
-//     0 0 40px #00baff,
-//     0 0 80px #00baff;
-
-// }
-
-/* .card {
-  padding: 2em;
-}
-
-.read-the-docs {
-  color: #888;
-} */
